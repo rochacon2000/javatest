68d9ae122d8238ca1a03e728da3289f247afcb70
Roberth <roberthchacon@cyberqp.com>
2025-10-02 15:00:05 -0600

    second commit

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+// This sample simulates a larger test suite
+for (var i = 0; i < 1000; i++) {
+
+	test(' test ' + i, async ({ page }) => {
+		await page.goto('https://playwright.dev/');
+		
+		// Click the get started link.
+		await page.getByRole('link', { name: 'Get started' }).click();
+		
+		// Expect the URL to contain intro.
+		await expect(page).toHaveURL(/.*intro/);
+	});
+}
