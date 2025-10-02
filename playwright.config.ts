d926b8ebab1cefbe590e46b0f56793724e442b41
Roberth <roberthchacon@cyberqp.com>
2025-10-02 17:52:45 -0600

    Update playwright.config.ts
    Uff fix the rror when running the tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,5 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
-  outputDir: "./test-results",
-  reporter: [
-    ['list'],
-    ['html', { outputFolder: 'playwright-report', open: 'never' }],
-  ],
+  outputDir: "./test-results"
 });
