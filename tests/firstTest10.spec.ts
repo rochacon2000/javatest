eaff2618fb80e104629d8a7b65298c728e5efb6d
Roberth <roberthchacon@cyberqp.com>
2025-10-02 20:10:43 -0600

    one file changed

--- tests/firstTest10.spec.ts
+++ tests/firstTest10.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test(' test 10', async ({ page }) => {
+test(' test > 10', async ({ page }) => {
 		await page.goto('https://playwright.dev/');
 		
 		// Click the get started link.
